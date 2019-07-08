562fc908d4aae3ca67af1c8bd4fa42fa1c59e14f
Ritwiz <ritwizme@gmail.com>
2019-07-08 15:20:57 +0530

    login signup form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { CardsComponent } from './components/cards/cards.component';
+import { LoginSignupComponent } from './components/login-signup/login-signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    CardsComponent
+    CardsComponent,
+    LoginSignupComponent
   ],
   imports: [
     BrowserModule,
